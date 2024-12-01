47f1ddd508b5ab0cbeefeb395fe0ffabf4413637
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-12-01 20:03:20 +0200

    update `ArduinoRadar.ino` with more comments and actually can send two sensors' info now

--- ArduinoRadar/ArduinoRadar.ino
+++ ArduinoRadar/ArduinoRadar.ino
@@ -1,62 +1,77 @@
-/*
-  This code controls a servo motor to rotate an ultrasonic sensor and measure distances. 
-  The sensor scans from 15 to 165 degrees and back, calculating the distance to objects 
-  and sending the angle and distance data to the serial port. The data can be used in the 
-  Processing IDE to visualize a real-time radar.
-  
-  Board: Arduino Uno R4 (or R3)
-  Component: Ultrasonic distance Sensor(HC-SR04)
-
-  Source Code From：https://howtomechatronics.com/projects/arduino-radar-project/
-*/
-
 // Includes the Servo library
 #include <Servo.h>.
 
+// Struct to carry info about a sensor
 struct sen {
-  bool enabled;
-  int trig;
-  int echo;
+  const bool enabled; // Used to define pin modes and for sent packages
+  const int trig;     // Sensor trigger pin
+  const int echo;     // Sensor echo pin
+  int distance;       // The distance the sensor detects
+
+  // Update the value of `distance`
+  // Only runs if sensor is enabled
+  void updateDistance() {
+    if (enabled) {
+      digitalWrite(trig, LOW);  // Set trig pin to low to ensure a clean pulse
+      delayMicroseconds(2);     // Delay for 2 microseconds
+      digitalWrite(trig, HIGH); // Send a 10 microsecond pulse by setting trig pin to high
+      delayMicroseconds(10);    // Delay for 10 microseconds
+      digitalWrite(trig, LOW);  // Set trig pin back to low
+
+      // Measure the pulse width of the echo pin and calculate the distance value
+      distance = pulseIn(echo, HIGH) / 58.00;  // Formula: (340m/s * 1us) / 2
+    }
+  }
 };
 
+/* ========================================================================== */
+
+// Serial setup
+const int serialPort = 9600;
+const bool sendNewLine = false;
+
+// Servo setup
+Servo myServo;
+const int servoPin = 2; //* CONFIGURE FOR YOURSELF IF YOU WANT
+
 // Defines Trig and Echo pins of the Ultrasonic Sensor 1
-const sen sensor1 = {
+//* CONFIGURE FOR YOURSELF IF YOU WANT
+sen sensor1 = {
   .enabled = true,
-  .trig = 11,
-  .echo = 12,
+  .trig = 3,
+  .echo = 4,
 };
 
 // Defines Trig and Echo pins of the Ultrasonic Sensor 2
-const sen sensor2 = {
+//* CONFIGURE FOR YOURSELF IF YOU WANT
+sen sensor2 = {
   .enabled = false,
-  .trig = 13,
-  .echo = 14,
+  .trig = 5,
+  .echo = 6,
 };
 
-// Servo setup
-Servo myServo; // Creates a servo object for controlling the servo motor
-const int servoPin = 10;
 // Servo angle range
+//* CONFIGURE FOR YOURSELF IF YOU WANT
 const int min_angle = 0;
 const int max_angle = 180;
 
-// Variables for the duration and the distance
-long duration;
-int distance;
+/* ========================================================================== */
 
 void setup() {
+  Serial.begin(serialPort); // Start serial connection as port `serialPort`
+  myServo.attach(servoPin); // Attaches the `servoPin` to the `myServo` object
+
   if (sensor1.enabled) {
-    pinMode(sensor1.trig, OUTPUT);  // Sets the trigPin as an Output
-    pinMode(sensor1.echo, INPUT);   // Sets the echoPin as an Input
+    // Set input and output of sensor 1
+    pinMode(sensor1.trig, OUTPUT);
+    pinMode(sensor1.echo, INPUT);
   }
 
   if (sensor2.enabled) {
-    pinMode(sensor2.trig, OUTPUT);  // Sets the trigPin as an Output
-    pinMode(sensor2.echo, INPUT);   // Sets the echoPin as an Input
+    // Set input and output of sensor 2
+    pinMode(sensor2.trig, OUTPUT);
+    pinMode(sensor2.echo, INPUT);
   }
-
-  Serial.begin(9600);
-  myServo.attach(servoPin);  // Defines on which pin is the servo motor attached
 }
 
 void loop() {
@@ -66,8 +81,9 @@ void loop() {
     delay(30);
 
     // Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
-    distance = calculateDistance(sensor1.trig, sensor1.echo);
-    sendPackage(i, distance);
+    sensor1.updateDistance();
+    sensor2.updateDistance();
+    sendPackage(i, sensor1.distance, sensor2.distance);
   }
 
   // Rotates the servo motor back from `max_angle` to `min_angle`
@@ -76,28 +92,29 @@ void loop() {
     delay(30);
 
     // Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
-    distance = calculateDistance(sensor1.trig, sensor1.echo);
-    sendPackage(i, distance);
+    sensor1.updateDistance();
+    sensor2.updateDistance();
+    sendPackage(i, sensor1.distance, sensor2.distance);
   }
 }
 
-void sendPackage(int angle, int distance) {
-  Serial.print(angle);     // Sends the current degree into the Serial Port
-  Serial.print(",");       // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
-  Serial.print(distance);  // Sends the distance value into the Serial Port
-  Serial.print(".");       // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
-  // Serial.print("\n");      // Sends a new line for readability
-}
+/* ========================================================================== */
 
-// Function to read the sensor data and calculate the distance
-int calculateDistance(int trig, int echo) {
-  digitalWrite(trig, LOW);   // Set trig pin to low to ensure a clean pulse
-  delayMicroseconds(2);      // Delay for 2 microseconds
-  digitalWrite(trig, HIGH);  // Send a 10 microsecond pulse by setting trig pin to high
-  delayMicroseconds(10);     // Delay for 10 microseconds
-  digitalWrite(trig, LOW);   // Set trig pin back to low
+void sendPackage(int angle, int distance1, int distance2) {
+  Serial.print(angle);        // Sends the current degree into the Serial Port
 
-  // Measure the pulse width of the echo pin and calculate the distance value
-  float distance = pulseIn(echo, HIGH) / 58.00;  // Formula: (340m/s * 1us) / 2
-  return distance;
+  if (sensor1.enabled) {
+    Serial.print(",");        // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+    Serial.print(distance1);  // Sends the distance value into the Serial Port
+  }
+
+  if (sensor2.enabled) {
+    Serial.print(",");        // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+    Serial.print(distance2);  // Sends the distance value into the Serial Port
+  }
+
+  Serial.print(".");          // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+
+  if (sendNewLine)
+    Serial.print("\n");         // Sends a new line for readability
 }
